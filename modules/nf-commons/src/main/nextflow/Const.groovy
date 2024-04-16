ada41be7c08dd5ce4730781944b9cea7fb848055
Ayush Kamat <ayush@latch.bio>
2024-04-16 15:15:09 -0700

    fix reserved name issue
    Signed-off-by: Ayush Kamat <ayush@latch.bio>

--- modules/nf-commons/src/main/nextflow/Const.groovy
+++ modules/nf-commons/src/main/nextflow/Const.groovy
@@ -57,12 +57,12 @@ class Const {
     /**
      * The app build time as linux/unix timestamp
      */
-    static public final long APP_TIMESTAMP = 1712780476723
+    static public final long APP_TIMESTAMP = 1713305360244
 
     /**
      * The app build number
      */
-    static public final int APP_BUILDNUM = 7005
+    static public final int APP_BUILDNUM = 7014
 
     /**
      * The app build time string relative to UTC timezone
