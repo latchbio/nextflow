767c3efe849c3baab13a9b7314df947fc6f41b9b
Rahul Desai <rahul@latch.bio>
2024-05-10 11:27:27 -0700

    add retry logic

--- modules/nextflow/src/main/groovy/nextflow/processor/TaskProcessor.groovy
+++ modules/nextflow/src/main/groovy/nextflow/processor/TaskProcessor.groovy
@@ -1834,6 +1834,11 @@ class TaskProcessor {
         // then add project bin dir
         if( executor.binDir )
             result.add(executor.binDir)
+
+        final sharedBinDir = System.getenv("LATCH_SHARED_BIN_DIR");
+        if ( sharedBinDir != null ) {
+            result.add(Path.of(sharedBinDir))
+        }
         return result
     }
 
