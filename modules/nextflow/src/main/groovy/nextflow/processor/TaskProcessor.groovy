475f947b5c227e86801dc32e8c0f34388c93db32
Rahul Desai <rahul@latch.bio>
2024-05-13 16:41:34 -0700

    few bug fixes to gql queries

--- modules/nextflow/src/main/groovy/nextflow/processor/TaskProcessor.groovy
+++ modules/nextflow/src/main/groovy/nextflow/processor/TaskProcessor.groovy
@@ -522,7 +522,7 @@ class TaskProcessor {
 
         // create process node
         Map res = client.execute("""
-            mutation CreateNode(\$executionId: String!, \$name: String!) {
+            mutation CreateNode(\$executionId: BigInt!, \$name: String!) {
                 createNfProcessNode(
                     input: {
                         nfProcessNode: {
@@ -541,9 +541,9 @@ class TaskProcessor {
                 executionId: executionId,
                 name: this.name,
             ]
-        )["nfProcessNode"] as Map
+        )["createNfProcessNode"] as Map
 
-        this.nodeId = (int) res.id
+        this.nodeId = (int) (res.nfProcessNode as Map).id
 
         // create process edges
         config.getInputs().each { it ->
@@ -700,9 +700,9 @@ class TaskProcessor {
                 processNodeId: this.nodeId,
                 index: task.index,
             ]
-        )["nfTaskInfo"] as Map
+        )["createNfTaskInfo"] as Map
 
-        task.taskId = (int) res.id
+        task.taskId = (int) (res.nfTaskInfo as Map).id
     }
 
     /**
