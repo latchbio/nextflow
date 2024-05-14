e732145a5817987b1e81b7e0cd7c160d6bf45148
Rahul Desai <rahul@latch.bio>
2024-05-14 12:32:08 -0700

    fix update task execution status

--- modules/nextflow/src/main/groovy/nextflow/processor/TaskRun.groovy
+++ modules/nextflow/src/main/groovy/nextflow/processor/TaskRun.groovy
@@ -894,7 +894,6 @@ class TaskRun implements Cloneable {
     }
 
     void updateTaskStatus(int attemptIdx, String status) {
-        return
         this.processor.client.execute("""
             mutation UpdateTaskStatus(\$taskId: BigInt!, \$attemptIdx: BigInt!, \$status: TaskExecutionStatus!) {
                 updateNfTaskExecutionInfoByTaskIdAndAttemptIdx(
@@ -905,7 +904,9 @@ class TaskRun implements Cloneable {
                         taskId: \$taskId,
                         attemptIdx: \$attemptIdx
                     }
-                )
+                ) {
+                    clientMutationId
+                }
             }
             """,
             [
