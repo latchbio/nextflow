468de49ef9825d90e2d1de3b3c456cf1faf9e3ad
Rahul Desai <rahul@latch.bio>
2024-06-12 16:39:44 -0700

    cleanup

--- modules/nf-httpfs/src/main/nextflow/file/http/LatchFileSystem.groovy
+++ modules/nf-httpfs/src/main/nextflow/file/http/LatchFileSystem.groovy
@@ -4,8 +4,6 @@ import java.nio.file.Path
 
 import groovy.util.logging.Slf4j
 
-import java.util.concurrent.ForkJoinPool
-
 @Slf4j
 class LatchFileSystem extends XFileSystem {
     LatchFileSystemProvider provider
