6fd2529153caf3efe70892bbd766d5dcd00c9314
Rahul Desai <rahul@latch.bio>
2024-06-12 15:06:50 -0700

    use a single thread pool across all LatchPaths

--- modules/nf-httpfs/src/main/nextflow/file/http/LatchFileSystem.groovy
+++ modules/nf-httpfs/src/main/nextflow/file/http/LatchFileSystem.groovy
@@ -4,16 +4,26 @@ import java.nio.file.Path
 
 import groovy.util.logging.Slf4j
 
+import java.util.concurrent.ForkJoinPool
+
 @Slf4j
 class LatchFileSystem extends XFileSystem {
     LatchFileSystemProvider provider
     String domain
+    ForkJoinPool executor
 
     LatchFileSystem(LatchFileSystemProvider provider, String domain) {
         super(provider, new URI("latch", domain, "/", null, null))
 
         this.provider = provider
         this.domain = domain
+
+        this.executor = new ForkJoinPool(
+            Math.max(10, Runtime.getRuntime().availableProcessors()*3),
+            ForkJoinPool.defaultForkJoinWorkerThreadFactory,
+            null,
+            true // async
+        )
     }
 
     @Override
