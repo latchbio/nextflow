fa6f639f1b39106f7969b752de338dea539cae3b
Rahul Desai <rahul@latch.bio>
2024-06-20 14:17:22 -0700

    better error handling when getting current workspace

--- modules/nf-commons/src/main/nextflow/Const.groovy
+++ modules/nf-commons/src/main/nextflow/Const.groovy
@@ -57,12 +57,12 @@ class Const {
     /**
      * The app build time as linux/unix timestamp
      */
-    static public final long APP_TIMESTAMP = 1718822325835
+    static public final long APP_TIMESTAMP = 1718916750505
 
     /**
      * The app build number
      */
-    static public final int APP_BUILDNUM = 6030
+    static public final int APP_BUILDNUM = 6034
 
     /**
      * The app build time string relative to UTC timezone
