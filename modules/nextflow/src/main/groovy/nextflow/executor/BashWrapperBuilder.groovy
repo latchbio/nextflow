a742bb89d6a692041fcc58487972795eb1496654
Taras Priadka <taras@latch.bio>
2024-09-09 15:29:33 -0700

    whitespace

--- modules/nextflow/src/main/groovy/nextflow/executor/BashWrapperBuilder.groovy
+++ modules/nextflow/src/main/groovy/nextflow/executor/BashWrapperBuilder.groovy
@@ -388,6 +388,7 @@ class BashWrapperBuilder {
         binding.fix_ownership = fixOwnership() ? "[ \${NXF_OWNER:=''} ] && (shopt -s extglob; GLOBIGNORE='..'; chown -fR --from root \$NXF_OWNER ${workDir}/{*,.*}) || true" : null
 
         binding.trace_script = isTraceRequired() ? getTraceScript(binding) : null
+
         return binding
     }
 
@@ -622,6 +623,7 @@ class BashWrapperBuilder {
     private String copyFileToWorkDir(String fileName) {
         copyFile(fileName, workDir.resolve(fileName))
     }
+
 
     String getCleanupCmd(String scratch) {
         String result = ''
