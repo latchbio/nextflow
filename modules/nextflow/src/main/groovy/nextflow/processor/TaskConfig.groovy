c0bae440936109e1388c87354ee0156f87723831
AidanAbd <aidan@latch.bio>
2025-08-26 10:44:21 -0700

    fix: add the boolean parsing

--- modules/nextflow/src/main/groovy/nextflow/processor/TaskConfig.groovy
+++ modules/nextflow/src/main/groovy/nextflow/processor/TaskConfig.groovy
@@ -545,12 +545,7 @@ class TaskConfig extends LazyMap implements Cloneable {
     }
 
     boolean getSpot() {
-        def value = get('spot')
-        if( value == null )
-            return false
-        if( value instanceof Boolean )
-            return value
-        return value.toString().toLowerCase() in Const.BOOL_YES
+        return toBool(get('spot'))
     }
 
     String getResourceLabelsAsString() {
